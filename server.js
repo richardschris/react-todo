1d2b8cb0a214e99d27e5d394abdf21a9c5fab3ce
Euphoria5L <christopher.james.richards@gmail.com>
2016-03-25 12:59:40 -0500

    added couchdb support
    Set up the Express API to access a CouchDB server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
+var cradle = require('cradle');
 
 app.use('/js', express.static('app'));
 app.use('/react', express.static('bower_components/react'));
@@ -9,6 +12,18 @@ app.use('/fetch', express.static('bower_components/fetch'));
 app.use('/es6-promise', express.static('bower_components/es6-promise'));
 app.use(bodyParser.json());
 
+cradle.setup({
+    host: '192.168.99.100',
+    cache: true,
+    raw: false,
+    forceSave: true,
+    port: 32768 // exposed docker port
+});
+
+var c = new(cradle.Connection)();
+var db = c.database('todo');
+
+var todoListLength = 0;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/app/index.html'));
@@ -21,28 +36,47 @@ var server = app.listen(3000, function() {
 var demoTodoList = [{index: 0, item: "eat food", isfinished: false}, {index: 1, item: "learn express", isfinished: true}];
 
 app.get('/api', function(req, res){
-    res.setHeader('Content-Type', 'application/json');
-    res.json(demoTodoList);
+    // TODO: Promisify the view!
+
+    let todoList = [];
+    db.view('items/allitems', function(err, doc) {
+        for (let item of doc) {
+            todoList.push(item['value']);
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.json(todoList);
+        todoListLength = todoList.length;
+    });
+
 });
 
 app.post('/api', function(req, res) {
-        
-    for (var i = 0; i < demoTodoList.length; i++) {
-        if (demoTodoList[i].index === req.body.index && demoTodoList[i].isfinished != req.body.isfinished) {
-            demoTodoList[i].isfinished = req.body.isfinished;
-            return;
-        }
-    }
 
-    demoTodoList.push(req.body);
+    db.save({index: req.body.index, item: req.body.item, isfinished: req.body.isfinished});
 });
 
 app.get('/api/:id(\\d+)/', function(req, res) {
-    res.send(demoTodoList[req.params.id]);
+    db.view('items/allitems', {key: Number(req.params.id)}, function(err, doc) {
+        res.setHeader('Content-Type', 'application/json');
+        res.json(doc);
+    });
 });
 
 app.post('/api/:id(\\d+)/', function(req, res) {
-    if (demoTodoList[req.params.id].isfinished != req.body.isfinished) { demoTodoList[req.params.id].isfinished = req.body.isfinished; }
+
+    db.view('items/allitems', {key: Number(req.params.id)}, function (err, doc)     {
+        console.log(doc[0]);
+        doc[0].value.isfinished = req.body.isfinished;
+        db.save(doc[0].id, doc[0].value);
+    });
+});
+
+// testing couchdb
+
+app.get('/test', function(req, res) {
+    db.view('items/allitems', function(err, doc) {
+    });
 });
 
 module.exports = server;
